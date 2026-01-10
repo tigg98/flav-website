baae159298abba7090b514d730e2545965fc0ca9
tigg98 <tiger.destefano@gmail.com>
2026-01-09 22:39:06 -0600

    browser tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,8 +75,8 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: "/logo.png",
+    apple: "/logo.png",
   },
 };
 
